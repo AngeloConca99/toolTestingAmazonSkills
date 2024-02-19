75e21deee441cbdf6b11aa411f4504149556acde
AngeloConca99 <a.conca@studenti.unimol.it>
2024-02-19 12:39:38 +0100

    caricamento automatico file funzionante

--- src/panels/HelloWorldPanel.ts
+++ src/panels/HelloWorldPanel.ts
@@ -7,7 +7,7 @@ export class HelloWorldPanel {
   public static currentPanel: HelloWorldPanel | undefined;
   private readonly _panel: vscode.WebviewPanel;
   private _disposables: vscode.Disposable[] = [];
-  private readonly timeoutMillis: number = 20000; 
+  private readonly timeoutMillis: number = 20000;
   private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
     this._panel = panel;
     this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
@@ -43,8 +43,8 @@ export class HelloWorldPanel {
   private _getWebviewContent(webview: vscode.Webview, extensionUri: vscode.Uri) {
     const displayHtmlContent = this.getDisplayHtmlContent(webview, extensionUri);
     const webviewUri = getUri(webview, extensionUri, ["out", "webview.js"]);
-    const stylesUri = this.getCss(webview,extensionUri);
-    const nonce = getNonce();   
+    const stylesUri = this.getCss(webview, extensionUri);
+    const nonce = getNonce();
     return /*html*/ `
        <!DOCTYPE html>
        <html lang="en">
@@ -62,82 +62,79 @@ export class HelloWorldPanel {
        </html>
        <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; font-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';">
      `;
-}
-private getDisplayHtmlContent(webview: vscode.Webview, extensionUri: vscode.Uri): string {
-  const fs = require('fs');
+  }
+  private getDisplayHtmlContent(webview: vscode.Webview, extensionUri: vscode.Uri): string {
+    const fs = require('fs');
     const path = require('path');
     try {
-        const displayDefault = require("../../out/display.js").default;
-        const htmlPath = path.join(extensionUri.fsPath, "out", displayDefault);
-        const displayHtmlContent = fs.readFileSync(htmlPath, 'utf-8');
-        return displayHtmlContent;
+      const displayDefault = require("../../out/display.js").default;
+      const htmlPath = path.join(extensionUri.fsPath, "out", displayDefault);
+      const displayHtmlContent = fs.readFileSync(htmlPath, 'utf-8');
+      return displayHtmlContent;
     } catch (error) {
-        console.error(error);
-        throw new Error('Errore durante il recupero del contenuto HTML');
+      console.error(error);
+      throw new Error('Errore durante il recupero del contenuto HTML');
     }
-}
-private getCss(webview: vscode.Webview, extensionUri: vscode.Uri): vscode.Uri {
-  const fs = require('fs');
+  } 6
+  private getCss(webview: vscode.Webview, extensionUri: vscode.Uri): vscode.Uri {
+    const fs = require('fs');
     const path = require('path');
     try {
-        const cssDefault = require("../../out/styles.js").default;
-        const cssUri= getUri(webview, extensionUri, ["out", cssDefault]);
-        return cssUri;
+      const cssDefault = require("../../out/styles.js").default;
+      const cssUri = getUri(webview, extensionUri, ["out", cssDefault]);
+      return cssUri;
     } catch (error) {
-        console.error(error);
-        throw new Error('Errore durante il recupero del contenuto Css');
+      console.error(error);
+      throw new Error('Errore durante il recupero del contenuto Css');
     }
-}
-private async findFilesWithTimeout(include: vscode.GlobPattern,exclude?: vscode.GlobPattern,maxResults?: number, timeoutMillis?: number): Thenable<vscode.Uri[]> {
-  const tokenSource = new vscode.CancellationTokenSource();
-  const timeout = timeoutMillis? setTimeout(() => tokenSource.cancel(), timeoutMillis) : undefined;
+  }
+  private async findFilesWithTimeout(include: vscode.GlobPattern, exclude?: vscode.GlobPattern, maxResults?: number, timeoutMillis?: number): Thenable<vscode.Uri[]> {
+    const tokenSource = new vscode.CancellationTokenSource();
+    const timeout = timeoutMillis ? setTimeout(() => tokenSource.cancel(), timeoutMillis) : undefined;
 
-  const filesPromise = vscode.workspace.findFiles(include, exclude, maxResults, tokenSource.token)
+    const filesPromise = vscode.workspace.findFiles(include, exclude, maxResults, tokenSource.token)
       .then((files) => {
-          clearTimeout(timeout); 
-          return files;
+        clearTimeout(timeout);
+        return files;
       });
 
-  return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       filesPromise.then(resolve, reject);
-  });
+    });
 
-}
+  }
 
-private _setWebviewMessageListener(webview: vscode.Webview) {
-  webview.onDidReceiveMessage(
-    async (message: any) => {
-      const command = message.command;
-      const text = message.text;
+  private _setWebviewMessageListener(webview: vscode.Webview) {
+    webview.onDidReceiveMessage(
+      async (message: any) => {
+        const command = message.command;
+        const text = message.text;
 
-      switch (command) {
-        case "start":
-          vscode.window.showInformationMessage(text);
-          return;
-        case "findFile":
-          vscode.window.showInformationMessage(text);
-          
-          try {
-            const jsonFile = await this.findFilesWithTimeout('**/skill-package/interactionModels/custom/*.json','**/node_modules/**',1,this.timeoutMillis);
-            if (jsonFile && jsonFile.length > 0) {
-              vscode.window.showInformationMessage("File trovato: " + jsonFile[0].fsPath);
-              console.log("File trovato: " + jsonFile);
-              webview.postMessage({ command: 'JsonFile', files: jsonFile });
-          } else {
-            
-              vscode.postMessage({ command: 'JsonFileNotFound' });
-              vscode.window.showErrorMessage('Nessun file JSON trovato.');
-             
-          }
-          } catch (error) {
-            vscode.window.showErrorMessage('Errore durante la ricerca dei file: ' + error);
-            webview.postMessage({ command: 'JsonFileNotFound' });
-          }
-          return;
-      }
-    },
-    undefined,
-    this._disposables
-  );
-}
+        switch (command) {
+          case "start":
+            vscode.window.showInformationMessage(text);
+            return;
+          case "findFile":
+
+            try {
+              const jsonFile = await this.findFilesWithTimeout('**/skill-package/interactionModels/custom/*.json', '**/node_modules/**', 1, this.timeoutMillis);
+              if (jsonFile && jsonFile.length > 0) {
+                webview.postMessage({ command: 'JsonFile', files: jsonFile });
+              } else {
+
+                webview.postMessage({ command: 'JsonFileNotFound' });
+                vscode.window.showErrorMessage('Nessun file JSON trovato.');
+
+              }
+            } catch (error) {
+              vscode.window.showErrorMessage('Errore durante la ricerca dei file: ' + error);
+              webview.postMessage({ command: 'JsonFileNotFound' });
+            }
+            return;
+        }
+      },
+      undefined,
+      this._disposables
+    );
+  }
 }
--- src/webview/main.mts
+++ src/webview/main.mts
@@ -2,7 +2,7 @@ import {
   provideVSCodeDesignSystem, vsCodeButton, Button,
   vsCodeDropdown, vsCodeOption, Dropdown, vsCodeProgressRing, vsCodeTextArea, ProgressRing
 } from "@vscode/webview-ui-toolkit";
- import { getUri } from "../utilities/getUri.js";
+import { getUri } from "../utilities/getUri.js";
 
 
 provideVSCodeDesignSystem().register(vsCodeButton(), vsCodeDropdown(), vsCodeOption(), vsCodeProgressRing(), vsCodeTextArea());
@@ -10,26 +10,45 @@ const vscode = acquireVsCodeApi();
 
 window.addEventListener('load', main);
 
-  function  main() {
+function main() {
   const input = document.getElementById('fileInput') as InputFile;
-  const slider = document.getElementById('slider')as Slider;
+  const slider = document.getElementById('slider') as Slider;
   const sliderValue = document.getElementById('sliderValue') as slidevalue;
   const startButton = document.getElementById('start') as Button;
   const dropdown = document.getElementById('dropdown') as Dropdown;
   dropdown?.addEventListener('input', handleDropdownChange);
   startButton?.addEventListener('click', handleStartClick);
   input?.addEventListener('change', handleFileSelect);
-  WebviewMessageListener();
- // findFile();
- }
-sliderValue.addEventListener('input', function() {
+
+  window.addEventListener('message', event => {
+    const message = event.data;
+    const command = message.command;
+    const files = message.files;
+    switch (command) {
+      case "JsonFile": {
+        seedLoading(files);
+      }
+        break;
+      case "JsonFileNotFound": {
+        progressRinghidden();
+      }
+        break;
+
+    }
+  },
+    undefined,
+    this._disposables
+  );
+  findFile();
+}
+sliderValue.addEventListener('input', function () {
   let value = parseFloat(sliderValue.value);
   value = Math.min(100, Math.max(0, value));
-  sliderValue.value=value;
+  sliderValue.value = value;
   slider.value = value.toString();
 });
 
-slider.addEventListener('input', function() {
+slider.addEventListener('input', function () {
   sliderValue.value = slider.value;
 });
 
@@ -39,34 +58,33 @@ function handleDropdownChange() {
 
   switch (selectedOption) {
     case 'option1':
-      
+
 
 
 
       break;
     case 'option2':
-      
+
 
 
       break;
     case 'option3':
-      
+
 
       break;
     default:
-     
-     }
+
+  }
 }
 
 function handleStartClick() {
-  vscode.postMessage({command:'findFile',text:"caricamento file"});
   vscode.postMessage({
     command: "start",
     text: "ciao"
   });
 }
-function progressRinghidden(){
-  const progressRing=document.getElementById('progressRing');
+function progressRinghidden() {
+  const progressRing = document.getElementById('progressRing');
   const input = document.getElementById('fileInput');
   progressRing?.classList.add('hidden');
   input?.classList.remove('hidden');
@@ -76,68 +94,43 @@ function handleFileSelect() {
   const input = document.getElementById('fileInput');
   input?.classList.add('hidden');
   progressRing?.classList.remove('hidden');
-  const file = input.files?.[0]; 
+  const file = input.files?.[0];
 
-  const reader = new FileReader(); 
-  reader.onload = function(event) {
-      const text = event.target?.result as string; 
-      textArea.value = text;
-      progressRing?.classList.add('hidden');
-      textArea.classList.remove('hidden');
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    const text = event.target?.result as string;
+    textArea.value = text;
+    progressRing?.classList.add('hidden');
+    textArea.classList.remove('hidden');
   };
 
-  reader.onerror = function(event) {
-    vscode.postMessage({command: "start",text: "errore Caricamento file"});
+  reader.onerror = function (event) {
+    vscode.postMessage({ command: "start", text: "errore Caricamento file" });
   };
 
   reader.readAsText(file);
 }
- function WebviewMessageListener() {
-  vscode.postMessage({ command: 'start',text:"caricato messageListnerwebui" });
-  window.addEventListener('message', event => {
-    const message = event.data;
-    const command = message.command;
-    const files = message.files;
-       switch (command) {
-        case "JsonFile":
-          vscode.postMessage({
-            command: "start",
-            text: "trovato"
-          });
-          seedLoading(files);
-          break;
-          case "JsonFileNotFound":
-            vscode.postMessage({
-              command: "start",
-              text: "non trovato"
-            });
-          progressRinghidden();
-          break;
-      
-        }
-    },
-    undefined,
-    this._disposables
-  );
+function WebviewMessageListener() {
+
 }
 function seedLoading(file) {
   const input = document.getElementById('fileInput');
-  try{
-    const progressRing=document.getElementById('progressRing');
+  try {
+    const progressRing = document.getElementById('progressRing');
     const textArea = document.getElementById('textContent');
     const jsonString = JSON.stringify(file, null, 2);
-    textArea.value=jsonString;
+    textArea.value = jsonString;
     progressRing?.classList.add('hidden');
     textArea?.classList.remove('hidden');
-  }catch (error){
+  } catch (error) {
     vscode.postMessage({
       command: "start",
       text: "errore caricamento"
     });
     progressRinghidden();
   }
- }
- function findFile(){
-  vscode.postMessage({command:'findFile',text:"caricamento file"});
+}
+function findFile() {
+  vscode.postMessage({ command: 'findFile' });
 }
 
