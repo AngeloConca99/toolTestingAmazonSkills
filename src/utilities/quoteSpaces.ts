057b97738abf77ca99a428133279c8a5d97c1d53
Mattia Natale <mattia.natale225@gmail.com>
2024-03-02 22:43:56 +0100

    spaces-in-path bug fix windows-linux

--- src/utilities/quoteSpaces.ts
+++ src/utilities/quoteSpaces.ts
@@ -1,8 +1,32 @@
+import os from 'os';
+
 export function quoteSpaces(path: string): string {
+    switch (os.platform()) {
+        case "win32":
+            return quoteSpacesWindows(path);
+        case "linux":
+            return quoteSpacesLinux(path);
+        default:
+            return path;
+    }
+    
+}
+
+function quoteSpacesWindows(path: string): string {
     // Controlla se la path contiene spazi
     if (path.includes(" ")) {
         // Aggiunge doppie virgolette alla path per gestire gli spazi in Windows
         return '"' + path + '"';
+    } else {
+        return path;
+    }
+}
+
+function quoteSpacesLinux(path: string): string {
+    // Controlla se la path contiene spazi
+    if (path.includes(" ")) {
+        // Aggiunge backslash alla path per gestire gli spazi in Linux
+        return path.replace(/ /g, "\\ ");
     } else {
         return path;
     }
